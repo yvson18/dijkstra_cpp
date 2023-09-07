8a3292cbc7044dcb27c02d508870f4c3875a49a9
Yvson Nunes <yvsonnunes7@gmail.com>
2023-09-07 19:01:48 -0300

    dijkstra working, but need some optimizations

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -20,6 +20,27 @@ struct Node {
 
 Dijkstra::Dijkstra(Graph graph){
     this->graph = graph;
+
+    // printar weights do grafo
+
+    // std::cout << "\nGrafo: " << std::endl;
+    // for(int i = 0; i < this->graph.numVertices; i++){
+    //     for(int j = 0; j < this->graph.numVertices; j++){
+    //         std::cout << this->graph.weights[i][j] << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
+
+    // // printar lista de adjacencia do grafo
+    // std::cout << "\nLista de Adjacencia: " << std::endl;
+
+    // for(int i = 0; i < this->graph.numVertices; i++){
+    //     std::cout << i << " -> ";
+    //     for(int j = 0; j < this->graph.adj[i].size(); j++){
+    //         std::cout << this->graph.adj[i][j] << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
 }
 
 std::vector<int> Dijkstra::solve(int source, int destiny){
@@ -56,24 +77,29 @@ std::vector<int> Dijkstra::solve(int source, int destiny){
     std::sort(fila_de_prioridade.begin(), fila_de_prioridade.end());
 
     while(!fila_de_prioridade.empty()){
-        int u = fila_de_prioridade[0].idx; // pega idx de quem tá no topo 
+        int u = fila_de_prioridade[0].idx; // pega idx de quem tá no topo
         
         for(int i = 0; i < this->graph.adj[u].size() ; i++){
             int v = this->graph.adj[u][i]; // v: idx do no filho
-            
+        
             if((!visitado[v]) && ((this->graph.weights[u][v] + chaves[u]) < chaves[v])){
                 // update pai e custos
                 pai[v] = u;
                 chaves[v] = this->graph.weights[u][v] + chaves[u];
+
+                //update fila de prioridade
+                for(int i = 0; i < fila_de_prioridade.size(); i++){
+                    if(fila_de_prioridade[i].idx == v)
+                        fila_de_prioridade[i].chave = chaves[v];
+                }
             }           
         }
-
+        
         fila_de_prioridade.erase(fila_de_prioridade.begin());
         
         //ordena fila de prioridade
         std::sort(fila_de_prioridade.begin(), fila_de_prioridade.end());
         visitado[u] = true;
-
     }
 
     //print uma tabela dizendo nó atual, pai, chave
--- main.cpp
+++ main.cpp
@@ -7,27 +7,37 @@
 int main() {
 
     // inicialização do grafo    
-    Graph graph(5);
+    // Graph graph(5);
+
+    // graph.addEdge(0, 1, 5);
+    // graph.addEdge(0, 2, 2);
+    // graph.addEdge(0, 3, 1);
+
+    // graph.addEdge(1, 0, 5);
+    // graph.addEdge(1, 4, 3);
+    // graph.addEdge(2, 0, 2);
+    // graph.addEdge(2, 4, 8);
+    // graph.addEdge(3, 0, 1);
+    // graph.addEdge(3, 4, 9);
+
+    // graph.addEdge(4, 1, 3);
+    // graph.addEdge(4, 2, 8);
+    // graph.addEdge(4, 3, 9);
+
+    Graph graph(4);
 
     graph.addEdge(0, 1, 5);
-    graph.addEdge(0, 2, 2);
-    graph.addEdge(0, 3, 1);
+    graph.addEdge(1, 2, 2);
+    graph.addEdge(2, 3, 3);
 
+    graph.addEdge(3, 2, 3);
+    graph.addEdge(2, 1, 2);
     graph.addEdge(1, 0, 5);
-    graph.addEdge(1, 4, 3);
-    graph.addEdge(2, 0, 2);
-    graph.addEdge(2, 4, 8);
-    graph.addEdge(3, 0, 1);
-    graph.addEdge(3, 4, 9);
-
-    graph.addEdge(4, 1, 3);
-    graph.addEdge(4, 2, 8);
-    graph.addEdge(4, 3, 9);
 
     //solver Dijkstra
     Dijkstra solver(graph);
 
-    std::vector<int> path = solver.solve(4,0);
+    std::vector<int> path = solver.solve(1,3);
 
     // printar caminho
     std::cout << "\nCaminho: " << std::endl;
